b9b87d4709e976039dc10db24ed2cef5e378be23
Ivan Gavrilin <ivan.gavrilin@inbox.ru>
2025-04-23 21:35:50 +0300

    feat: implement fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,29 @@
+import { writeFile, access } from "node:fs/promises";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
 const create = async () => {
-    // Write your code here 
+  const content = "I am fresh and young";
+  const folderName = "files";
+  const fileName = "fresh.txt";
+
+  const targetFolder = join(
+    dirname(fileURLToPath(import.meta.url)),
+    folderName
+  );
+  const file = join(targetFolder, fileName);
+
+  try {
+    await access(file);
+
+    throw new Error(`File already exists`);
+  } catch (error) {
+    if (error.message === `File already exists`) {
+      throw new Error("FS operation failed");
+    }
+  }
+
+  await writeFile(file, content);
 };
 
 await create();
