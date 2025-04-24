03a3c7cde95212ba0368a5b83e515f205e4b2384
Ivan-Gav <ivan.gavrilin@inbox.ru>
2025-04-24 16:23:31 +0300

    feat: implement fs rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
+import { access, rename as renameFile } from "node:fs/promises";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
 const rename = async () => {
-    // Write your code here 
+  const _dirname = dirname(fileURLToPath(import.meta.url));
+
+  const sourceFile = join(_dirname, "files", "wrongFilename.txt");
+  const targetFile = join(_dirname, "files", "properFilename.md");
+
+  try {
+    await access(sourceFile);
+  } catch {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    await access(targetFile);
+    throw new Error("file already exists");
+  } catch (e) {
+    if (e.message === "file already exists") {
+      throw new Error("FS operation failed");
+    }
+  }
+
+  await renameFile(sourceFile, targetFile);
 };
 
 await rename();
