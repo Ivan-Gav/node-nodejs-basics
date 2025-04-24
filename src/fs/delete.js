b57bc365ecbaf93415df7e4eb9298dde77194936
Ivan-Gav <ivan.gavrilin@inbox.ru>
2025-04-24 17:04:28 +0300

    feat: implement fs delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { unlink } from "node:fs/promises";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import { throwErrorIfNotExists } from "./utils.js";
+
 const remove = async () => {
-    // Write your code here 
+  const _dirname = dirname(fileURLToPath(import.meta.url));
+  const fileToRemove = join(_dirname, "files", "fileToRemove.txt");
+
+  await throwErrorIfNotExists(fileToRemove);
+
+  await unlink(fileToRemove);
 };
 
 await remove();
