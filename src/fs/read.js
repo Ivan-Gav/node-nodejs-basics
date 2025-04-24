b564f0a4e6f427dd6282e4e814869f2155ef8fe1
Ivan-Gav <ivan.gavrilin@inbox.ru>
2025-04-24 17:25:30 +0300

    feat: implement fs read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { readFile } from "node:fs/promises";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+import { throwErrorIfNotExists } from "./utils.js";
+
 const read = async () => {
-    // Write your code here 
+  const _dirname = dirname(fileURLToPath(import.meta.url));
+
+  const sourceFile = join(_dirname, "files", "fileToRead.txt");
+
+  await throwErrorIfNotExists(sourceFile);
+
+  const content = await readFile(sourceFile, "utf8");
+  console.log(content);
 };
 
 await read();
