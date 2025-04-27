568a688c1d075dbb7e7eb1bf7bfc7bec842c4950
Ivan Gavrilin <ivan.gavrilin@inbox.ru>
2025-04-27 15:56:34 +0300

    feat: implement wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
 const performCalculations = async () => {
-    // Write your code here
+  const _dirname = dirname(fileURLToPath(import.meta.url));
+
+  const workerFile = join(_dirname, "worker.js");
+
+  const numCPUs = cpus().length;
+
+  const workers = [];
+
+  for (let i = 0; i < numCPUs; i++) {
+    const workerPromise = new Promise((resolve) => {
+      const worker = new Worker(workerFile);
+
+      worker.once("message", (data) => {
+        resolve({ status: "resolved", data });
+      });
+
+      worker.once("error", () => {
+        resolve({ status: "error", data: null });
+      });
+
+      worker.postMessage(10 + i);
+    });
+
+    workers.push(workerPromise);
+  }
+
+  const results = await Promise.all(workers);
+
+  console.log(results);
 };
 
 await performCalculations();
