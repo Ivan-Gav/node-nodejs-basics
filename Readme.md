a55b81c420383e65e6764b922fd2bc9326e2c801
Ivan Gavrilin <ivan.gavrilin@inbox.ru>
2025-04-27 13:59:01 +0300

    feat: implement zip compress and decompress

--- Readme.md
+++ Readme.md
@@ -30,8 +30,8 @@
 
 ### Zlib (src/zip)
 
-- [ ] compress.js - implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
-- [ ] decompress.js - implement function that decompresses archive.gz back to the fileToCompress.txt with same content as before compression using zlib and Streams API
+- [x] compress.js - implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+- [x] decompress.js - implement function that decompresses archive.gz back to the fileToCompress.txt with same content as before compression using zlib and Streams API
 
 ### Worker Threads (src/wt)
 
--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "node:zlib";
+import { pipeline } from "stream/promises";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
 const compress = async () => {
-    // Write your code here 
+  const _dirname = dirname(fileURLToPath(import.meta.url));
+
+  const sourceFile = join(_dirname, "files", "fileToCompress.txt");
+  const targetFile = join(_dirname, "files", "archive.gz");
+
+  const readStream = createReadStream(sourceFile);
+  const gzipStream = createGzip();
+  const writeStream = createWriteStream(targetFile);
+
+  await pipeline(readStream, gzipStream, writeStream);
 };
 
 await compress();
